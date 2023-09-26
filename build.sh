80c3f9d9be1c182664898e37366e630580ab9e92
isac <jeandersantos0310@gmail.com>
2023-09-26 11:36:09 -0300

    package

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-// build.sh
-#!/usr/bin/env bash
+# build.sh
 # exit on error
 set -o errexit
 
+echo "Running 'yarn'..."
 yarn
+
+echo "Running 'yarn build'..."
 yarn build
+
+echo "Running 'yarn typeorm migration:run -d dist/data-source'..."
 yarn typeorm migration:run -d dist/data-source
